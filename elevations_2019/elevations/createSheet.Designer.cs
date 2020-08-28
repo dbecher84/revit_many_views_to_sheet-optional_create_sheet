36f3a7b044bad817af62faffe83f7dae64ffbcaf
dbecher84 <dbecher84@gmail.com>
2020-08-28 15:56:08 -0400

    Add files via upload

---
+++ elevations_2019/elevations/createSheet.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace sheetcreation
+{
+    partial class createSheet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(118, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 71);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Create a new sheet for the slected Views?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(47, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "No";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(198, 27);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(43, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Yes";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(47, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(290, 77);
+            this.panel1.TabIndex = 3;
+            // 
+            // createSheet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 250);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "createSheet";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Create New Sheet";
+            this.Load += new System.EventHandler(this.createSheet_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ elevations_2019/elevations/viewsToSheet.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace get_sheet
+{
+    public partial class viewsToSheet : Form
+    {
+        List<string> fullList = new List<string>();
+
+        public viewsToSheet(List<string> list)
+        {
+            InitializeComponent();
+
+            foreach (string item in list)
+            {
+                //MessageBox.Show(item);
+                listBox1.Items.Add(item);
+                fullList.Add(item);
+            }
+        }
+
+        public List<string> sheetForViews { get; set; }
+
+        //create view list to send for export
+        public List<string> CreateSheetList()
+        {
+            List<string> sheetListWorking = new List<string>();
+            if (listBox2.Items != null)
+            {
+                foreach (var item in listBox2.Items)
+                {
+                    string text = item.ToString();
+                    //MessageBox.Show(text);
+                    sheetListWorking.Add(text);
+                }
+                sheetForViews = sheetListWorking;
+                return sheetForViews;
+            }
+            else
+            {
+                MessageBox.Show("List Box was empty");
+                return sheetForViews;
+            }
+        }
+
+        private void viewsToSheet_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //add item to second list on double click
+        private void DoubleClick1(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                listBox2.Items.Clear();
+                listBox2.Items.Add(listBox1.SelectedItem);
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //clear list 2
+        private void ClearAll(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //view list to filter list box 1 
+        private void Filter_Load(object sender, EventArgs e)
+        {
+            string uText = textBox1.Text;
+            string cText = uText.Trim();
+            //MessageBox.Show(cText);
+            List<string> filteredList = new List<string>();
+            foreach (string item in listBox1.Items)
+            {
+                if (item.Contains(cText))
+                {
+                    filteredList.Add(item);
+                }
+            }
+            listBox1.Items.Clear();
+            foreach (string item in filteredList)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CreateSheetList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (string item in fullList)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
